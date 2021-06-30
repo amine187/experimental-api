1c19cde021d09e176a5c0744e378fe8f664e4c32
amine187 <amin.rhouma@gmail.com>
2021-06-30 13:02:42 +0200

    add Firebase config

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express');
+
+const firebase = require('firebase');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCOGh6Zoq4xMepOlVH-ocbQJcwb4OOWJkU',
+  authDomain: 'api-project-250622595269.firebaseapp.com',
+  databaseURL: 'https://api-project-250622595269.firebaseio.com',
+  projectId: 'api-project-250622595269',
+  storageBucket: 'api-project-250622595269.appspot.com',
+  messagingSenderId: '250622595269',
+  appId: '1:250622595269:web:799d57b3839f723f6f60ac',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
